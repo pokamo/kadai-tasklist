58103c849d31e9d8731d4f99cb5719ee661e5e06
pokamo <ppokamo@gmail.com>
2021-11-10 12:59:25 +0000

    user login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,20 +9,22 @@
 
     <body>
         <header class="mb-4">
-            <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-                {{-- トップページへのリンク --}}
-                <a class="navbar-brand" href="/">Tasklist</a>
+            <!--<nav class="navbar navbar-expand-sm navbar-dark bg-dark">-->
+            <!--    {{-- トップページへのリンク --}}-->
+            <!--    <a class="navbar-brand" href="/">Tasklist</a>-->
 
-                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+            <!--    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">-->
+            <!--        <span class="navbar-toggler-icon"></span>-->
+            <!--    </button>-->
 
-                <div class="collapse navbar-collapse" id="nav-bar">
-                    <ul class="navbar-nav mr-auto"></ul>
-                    <ul class="navbar-nav">
-                    </ul>
-                </div>
-            </nav>
+            <!--    <div class="collapse navbar-collapse" id="nav-bar">-->
+            <!--        <ul class="navbar-nav mr-auto"></ul>-->
+            <!--        <ul class="navbar-nav">-->
+            <!--        </ul>-->
+            <!--    </div>-->
+            <!--</nav>-->
+        {{-- ナビゲーションバー --}}
+        @include('commons.navbar')
         </header>
 
         <div class="container">
